741f880097462f5f7f68704035e3e6ee29a7d5e6
Kyung Baek <kbaek11@live.unc.edu>
2018-02-27 15:10:37 -0500

    Added changes

--- wsgi.py
+++ wsgi.py
@@ -1,9 +1,15 @@
-from flask import Flask
-application = Flask(__name__)
+from flask import Flask, render_template, request
+app= Flask(__name__)
 
-@application.route("/")
+@app.route('/')
 def hello():
-    return "Hello World!"
+    return render_template('hello.html')
+
+@app.route('/result',methods = ['POST', 'GET'])
+def result():
+   if request.method == 'POST':
+      result = request.form
+      return render_template("result.html",result = result)
 
 if __name__ == "__main__":
-    application.run()
+    app.run(debug = True)
