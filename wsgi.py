ae69438730ffa507a2dda5d30ca2d10c092f7b7d
Kyung Baek <kbaek11@live.unc.edu>
2018-02-27 15:34:09 -0500

    Changed  variable app to application

--- wsgi.py
+++ wsgi.py
@@ -1,15 +1,15 @@
 from flask import Flask, render_template, request
-app= Flask(__name__)
+application = Flask(__name__)
 
-@app.route('/')
+@application.route('/')
 def hello():
     return render_template('hello.html')
-
-@app.route('/result',methods = ['POST', 'GET'])
+application
+@application.route('/result',methods = ['POST', 'GET'])
 def result():
    if request.method == 'POST':
       result = request.form
       return render_template("result.html",result = result)
 
 if __name__ == "__main__":
-    app.run(debug = True)
+    application.run(debug = True)
